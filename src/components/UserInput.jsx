aee9dfd534ae1c006b3f49f85a9d1de20b112dd1
GiorgioBolzoni <gbolzoni96@gmail.com>
2024-03-14 12:06:03 +0100

    creata base componente UserInput

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,3 +1,26 @@
 export default function UserInput(){
-    return 
+    return (
+        <>
+                <div id="user-input" className="input-group">
+                    <label htmlFor="input-field">
+                        <div>INITIAL INVESTMENT</div>
+                        <input type="text"/>
+                    </label>
+                    <label htmlFor="input-field">
+                        <div>ANNUAL INVESTMENT</div>
+                        <input type="text"/>
+                    </label>
+                    <label htmlFor="input-field">
+                        <div>EXPECTED RETURN</div>
+                        <input type="text"/>
+                    </label>
+                    <label htmlFor="input-field">
+                        <div>DURATION</div>
+                        <input type="text"/>
+                    </label>
+
+                    
+                </div>
+        </>
+    )
 }
